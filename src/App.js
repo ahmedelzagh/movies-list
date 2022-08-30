b6eb373936b0bab531af015289106e40246547c3
Ahmed Elzagh <ahmedelzagh50@gmail.com>
2022-08-30 18:41:57 +0200

    using async/await

--- src/App.js
+++ src/App.js
@@ -8,19 +8,20 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const getMoviesHandler = () => {
+  const getMoviesHandler = async () => {
     setIsLoading(true);
-    fetch("https://swapi.py4e.com/api/films")
-      .then((res) => res.json())
-      .then((data) => {
-        const transformData = data.results.map((movie) => {
-          return { id: movie.episode_id, title: movie.title, openingText: movie.opening_crawl, releaseDate: movie.release_date };
-        });
-
-        setMovies(transformData);
-        setIsLoading(false);
-      })
-      .catch((err) => err && setError(true));
+    const res = await fetch("https://swapi.py4e.com/api/film");
+    if (res.ok) {
+      const data = await res.json();
+      console.log(data);
+      const transformData = data.results.map((movie) => {
+        return { id: movie.episode_id, title: movie.title, openingText: movie.opening_crawl, releaseDate: movie.release_date };
+      });
+      setMovies(transformData);
+      setIsLoading(false);
+    } else {
+      setError(true);
+    }
   };
 
   return (
